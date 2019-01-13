6f49602cfa3d5fdaa764b2dbca4280b43910f76f
Spencer Shupe <shupe@protonmail.com>
2019-01-13 14:55:32 -0800

    Refactor for typescript conventions

--- Suggestion 1 - SQL Query/src/EmailVacationGrant.ts
+++ Suggestion 1 - SQL Query/src/EmailVacationGrant.ts
@@ -1,10 +1,10 @@
-import { EmployeeVacationGrant, EmailApi } from './interfaces';
+import { IEmployeeVacationGrant, IEmailApi } from './interfaces';
 
 import knexClient from './db/dbClient';
 // We haved decided to grant bonus vacation to every employee, 1 day per year of experience
 // we need to email them a notice
 function EmailVacationGrant(
-  emailApi: EmailApi
+  emailApi: IEmailApi
 ) {
   // SELECT AddressBook.email, WorkHistory.name, WorkHistory.years, payroll.vacationDays 
   // from AddressBook 
@@ -20,8 +20,8 @@ function EmailVacationGrant(
     .from('workhistory')
     .join('addressbook', 'addressbook.empno', '=', 'workhistory.empno')
     .join('payroll', 'addressbook.empno', '=', 'payroll.empno')
-    .then(rows => {
-      rows.map( ({email, name, yearsEmployed, vacationDays}: EmployeeVacationGrant) => {
+    .then( (rows: any[]) => {
+      rows.map( ({email, name, yearsEmployed, vacationDays}: IEmployeeVacationGrant) => {
         let newVacationBalance = yearsEmployed + vacationDays;
         let emailBody =  `Dear ${name},\n` +
           `based on your ${yearsEmployed} ${yearsEmployed === 1 ? 'year' : 'years'} of employment, you have been granted ${yearsEmployed} ${yearsEmployed === 1 ? 'day' : 'days'} of vacation, bringing your total to ${newVacationBalance}.`;
@@ -29,7 +29,7 @@ function EmailVacationGrant(
         emailApi.sendEmail(email, emailBody)
       })
     })
-    .catch(e => console.error(e))
+    .catch( (e: Error) => console.error(e))
     .finally(() => knexClient.destroy());
 }
 
